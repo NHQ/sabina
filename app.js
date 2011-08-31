fae21cf1cf876664a1903959e0812365a45f9d84
root <root@li79-52.members.linode.com>
2011-08-31 04:11:58 -0400

    hoy

--- app.js
+++ app.js
@@ -60,30 +60,49 @@ app.post('/picload', function(req, res){
 	});
 });
 
+function pickles(x,size){
+    _.each(x, function(e){
+        request.get(e.url).pipe(fs.createWriteStream('public/images/'+size+e.name))    
+    })
+}
+
+app.get('/', function(req, res){
+        	res.render('home', {layout: false, title: 'PLD Custom Home Builders'})
+})
+
 app.post('/uploads', function (req, res){
 	var info = JSON.parse(req.body.transloadit);
-	console.log(info);
-		var _id = info.fields._id || null;
+    var ids = JSON.parse(fs.readFileSync('public/json/ids.json'));
+    if (_.include(ids, info.assembly_id)){return};
+    ids.push(info.assembly_id);
+    fs.writeFileSync('public/json/ids.json', JSON.stringify(ids))
+    console.log(info);
+    console.log(info.results);
+	var _id = info.fields._id || null;
 	
 // get the image files
 
 _.each(info.uploads, function (e){
-	request.get(e.url).pipe(fs.createWriteStream('public/images/large_'+e.name))})
+	var tree = request.get(e.url).pipe(fs.createWriteStream('public/images/large_'+e.name))})
 	
 if (info.results.thumb){
 	_.each(info.results.thumb, function (e) {
-		request.get(e.url).pipe(fs.createWriteStream('public/images/thumb_'+e.name))})}
+	var p =	request.get(e.url).pipe(fs.createWriteStream('public/images/thumb_'+e.name))})}
 
 if (info.results.medium){
 	_.each(info.results.medium, function (e) {
-	  request.get(e.url).pipe(fs.createWriteStream('public/images/medium_'+e.name))})}
-
+	var t =  request.get(e.url).pipe(fs.createWriteStream('public/images/medium_'+e.name))})}
 // write to gallery.json
 	if (info.fields.section)
 		{
 				var gallery = JSON.parse(fs.readFileSync('public/json/gallery.json'));
-				gallery[info.fields.section].push({'large': 'images/large_'+info.uploads[0].name, 'medium': 'images/medium_'+info.uploads[0].name, 'thumb':'images/thumb_'+info.uploads[0].name})
-				fs.writeFile("public/json/gallery.json", JSON.stringify(gallery), function(e,r){
+                var added = {};
+                added.large = 'images/large_'+info.uploads[0].name;
+                if (info.results.thumb) added.thumb =  'images/thumb_'+info.results.thumb[0].name;
+                if (info.results.medium) added.medium = 'images/medium_'+info.results.medium[0].name;
+				gallery[info.fields.section].push(added)
+				
+                fs.writeFile("public/json/gallery.json", JSON.stringify(gallery), function(e,r){
 					console.log(e || "no error")
 				});
 		}
@@ -92,7 +111,11 @@ if (info.results.medium){
 
 	if(_id){
 		var portfolio = JSON.parse(fs.readFileSync('public/json/portfolio.json'));
-		portfolio[_id] = _.extend(portfolio[_id], {'large': 'images/large_'+info.uploads[0].name, 'medium': 'images/medium_'+info.uploads[0].name, 'thumb':'images/thumb_'+info.uploads[0].name})
+        var tadded = {img:{}};
+        tadded.img.large = 'images/large_'+info.uploads[0].name;
+        if (info.results.thumb) tadded.img.thumb = 'images/thumb_'+info.results.thumb[0].name;
+        if (info.results.medium) tadded.img.medium = 'images/medium_'+info.results.medium[0].name;
+		portfolio[_id] = _.extend(portfolio[_id], tadded)
 		fs.writeFile("public/json/portfolio.json", JSON.stringify(portfolio), function(e,r){
 			console.log(e || "no error")
 		});
@@ -108,6 +131,84 @@ app.get('/portfolio', function(req,res){
 			}})
 	})
 })
+
+app.post('/new/bldg', function(req,res){
+    console.log(req.body);
+	var _id = req.body._id;
+	var portfolio = JSON.parse(fs.readFileSync('public/json/portfolio.json'));
+	console.log(portfolio[_id]);
+	_.extend(portfolio[_id], req.body);
+		console.log(portfolio[_id]);
+	fs.writeFile("public/json/portfolio.json", JSON.stringify(portfolio), function(e,r){
+		console.log(e || "no error")
+        res.redirect('/edit/bldg?id='+_id);
+	});
+})
+app.get('/edit/portfolio', function(req,res){
+    fs.readFile('public/json/portfolio.json', function(e,r){
+		console.log(e+"\n"+r);
+		res.render('editPortfolio', {layout: false, title: 'PLD Custom Home Builders', locals: {
+			bldgs: JSON.parse(r)
+			}})
+	})
+})
+app.get('/del/bldg', function(req,res){
+    var b = JSON.parse(fs.readFileSync('public/json/portfolio.json'));
+    b.splice(req.query.id,1)
+    fs.writeFile('public/json/portfolio.json', JSON.stringify(b), function(e,r){
+    	console.log(e+"\n"+r);
+    })
+})
+
+app.get('/edit/bldg', function(req, res){
+    var bldg = {
+		address: "",
+		description: "",
+		status: ""
+	};
+    var _id = req.query.id;
+	console.log(_id);
+    var bldg = JSON.parse(fs.readFileSync('public/json/portfolio.json'))[_id];
+	res.render('editBldg', {layout: false, title: 'New Building', locals: {
+		bldg: bldg,
+        id: _id,
+		tranny: {
+  			"auth": 
+			{
+    			"key": "08f81b65f9c4433796ca6f17861f57bf"
+  			},
+ 			"template_id": "b01e9ea666c741a4bc428d8b783b161d",
+			"notify_url": "http://74.207.237.52:3003/uploads?_id="+_id
+		}
+		}})
+})
+
+app.get('/new/bldg', function(req, res){
+	var bldg = {
+		address: "",
+		description: "",
+		status: ""
+	};
+	var portfolio = JSON.parse(fs.readFileSync('public/json/portfolio.json'));
+	var _id = portfolio.length;
+	portfolio[_id] = {};
+	fs.writeFile('public/json/portfolio.json', JSON.stringify(portfolio), function(e,r){
+				console.log(e+"\n"+r);
+	})
+	res.render('newBldg', {layout: false, title: 'New Building', locals: {
+		bldg: bldg,
+		id: _id, // index
+		tranny: {
+  			"auth": 
+			{
+    			"key": "08f81b65f9c4433796ca6f17861f57bf"
+  			},
+ 			"template_id": "b01e9ea666c741a4bc428d8b783b161d",
+			"notify_url": "http://74.207.237.52:3003/uploads?_id="+_id
+		}
+		}})
+})
+
 
 app.get('/about', function(req,res){
 	fs.readFile('public/json/about.json',encoding='utf8', function(e,r){
@@ -150,7 +251,6 @@ app.get('/edit/gallery', function(req,res){
 		var content = JSON.parse(r);
 		var section = req.query.section || Object.keys(content)[0];
 				console.log(e+"\n"+r);
-				console.log(content[section])
 		res.render('editGallery', {layout: false, title: 'PLD Custom Home Builders', locals: {
 			section: section,
 			sections: Object.keys(content),
@@ -160,9 +260,8 @@ app.get('/edit/gallery', function(req,res){
 				{
 	    			"key": "08f81b65f9c4433796ca6f17861f57bf"
 	  			},
-				"redirect_url": "http://74.207.237.52:3000/edit/gallery?section="+section+"&",
 	 			"template_id": "b01e9ea666c741a4bc428d8b783b161d",
-				"notify_url": "http://74.207.237.52:3000/uploads"
+				"notify_url": "http://74.207.237.52:3003/uploads"
 			}
 			}})
 	})
@@ -181,54 +280,20 @@ app.post('/del/section', function(req,res){
 	res.redirect('/edit/gallery')
 })
 app.post('/del/image', function(req,res){
-	var d = JSON.parse(fs.readFileSync('public/json/gallery.json'))[req.body.section];
-	delete d[req.body.index];
-	fs.writeFile('public/json/gallery.json', JSON.stringify(d));
-	res.redirect('/edit/gallery')
+    console.log(req.body);
+	var d = JSON.parse(fs.readFileSync('public/json/gallery.json'));
+    d[req.body.section].splice([req.body.index],1)
+    console.log(d);
+    fs.writeFile('public/json/gallery.json', JSON.stringify(d));
+	res.redirect('/edit/gallery?section='+req.body.section)
 })
 app.get('/images', function(req,res){
 	fs.readdir('public/images', function(e,r){
 		res.send(r);
 	})
 })
-app.post('/new/bldg', function(req,res){
-	console.log(req.body);
-	var _id = req.body._id;
-	var portfolio = JSON.parse(fs.readFileSync('public/json/portfolio.json'));
-	console.log(portfolio[_id]);
-	_.extend(portfolio[_id], req.body);
-		console.log(portfolio[_id]);
-	fs.writeFile("public/json/portfolio.json", JSON.stringify(portfolio), function(e,r){
-		console.log(e || "no error")
-	});
-})
-
-app.get('/new/bldg', function(req, res){
-	var bldg = {
-		address: "",
-		description: "",
-		status: ""
-	};
-	var portfolio = JSON.parse(fs.readFileSync('public/json/portfolio.json'));
-	var _id = portfolio.length;
-	portfolio[_id] = {};
-	fs.writeFile('public/json/portfolio.json', JSON.stringify(portfolio), function(e,r){
-				console.log(e+"\n"+r);
-	})
-	res.render('bldg', {layout: false, title: 'New Building', locals: {
-		bldg: bldg,
-		id: _id, // index
-		tranny: {
-  			"auth": 
-			{
-    			"key": "08f81b65f9c4433796ca6f17861f57bf"
-  			},
- 			"template_id": "b01e9ea666c741a4bc428d8b783b161d",
-			"notify_url": "http://74.207.237.52:3000/uploads?_id="+_id
-		}
-		}})
-})
 
 
-app.listen(3000);
+
+app.listen(3003);
 console.log("Express server listening on port %d", app.address().port);
--- public/js/pld.js
+++ public/js/pld.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
 	$('#galleryForm').transloadit({
 	  wait: true,
-	  autoSubmit: true,
-	  fields: true
+	  autoSubmit: false,
+	  fields: true,
+      onResult: function (step, result){
+          console.log(step+"\n"+result)
+      }
 	})
 
 	$('#MyForm').transloadit({
 	  wait: true,
-	  autoSubmit: true,
-	  fields: true
+	  autoSubmit: false,
+	  fields: true,
+        onResult: function (step, result){
+            ayaya = result;
+          console.log(step+"\n"+result)
+          $("#uploaded").empty().append("<img src="+result.url+" class=preview />").width(600)
+      }
 	})
 })
